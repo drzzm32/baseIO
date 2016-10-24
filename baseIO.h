63a1fd8b356c1170a98adf7c5814b57af890605f
drzzm32 <drzzm32@gmail.com>
2016-10-24 21:01:55 +0800

    init

---
+++ baseIO.h
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <vadefs.h>
+#include <string.h>
+
+#if defined(WINDOWS)
+#define CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#define clear() system("cls")
+int print(const char* format, ...) {
+	va_list args = 0;
+	__crt_va_start(args, format);
+	int result = vprintf(format, args);
+	__crt_va_end(args);
+	return result;
+}
+int scan(char* buffer) { return gets(buffer); }
+int fscan(char* buffer, const char* format, ...) {
+	gets(buffer);
+	va_list args = 0;
+	__crt_va_start(args, format);
+	int result = vsscanf(buffer, format, args);
+	__crt_va_end(args);
+	return result;
+}
+
+int main(int argc, char* argv) {
+	int result;
+	result = emuasm(argc, argv);
+	system("pause");
+	return result;
+}
+
+#elif defined(LINUX)
+#include <stdlib.h>
+#define clear() system("clear")
+int print(const char* format, ...) {
+	va_list args = 0;
+	va_start(args, format);
+	int result = vprintf(format, args);
+	va_end(args);
+	return result;
+}
+int scan(char* buffer) { return gets(buffer); }
+int fscan(char* buffer, const char* format, ...) {
+	gets(buffer);
+	va_list args = 0;
+	va_start(args, format);
+	int result = vsscanf(buffer, format, args);
+	va_end(args);
+	return result;
+}
+
+int main(int argc, char* argv) {
+	int result;
+	result = emuasm(argc, argv);
+	return result;
+}
+
+#elif defined(ARDUINO)
+#define IOBUF 128
+#define BACKCOLOR 0x0000
+#define clear() { tft.setCursor(0, 0); tft.fillScreen(BACKCOLOR); }
+int print(const char* format, ...) {
+	char* iobuf = malloc(sizeof(char) * IOBUF);
+	va_list args = 0;
+	va_start(args, format);
+	int result = vsprintf(iobuf, format, args);
+	va_end(args);
+	tft.print(iobuf);
+	free(iobuf);
+	return result;
+}
+int scan(char* buffer) {
+	char count = 0;
+	while (true) {
+		if (Serial.available() > 0) {
+			if (Serial.peek() == '\n') break;
+			buffer[count] = Serial.read();
+			if (buffer[count] == 0x08 && count > 0) {
+				count -= 1;
+				tft.print((char)0x08);
+				continue;
+			}
+			else if (buffer[count] != 0x08) {
+				tft.print(buffer[count]);
+				count += 1;
+			}
+		}
+		else if (Serial1.available() > 0) {
+			if (Serial1.peek() == '\n') break;
+			buffer[count] = Serial1.read();
+			if (buffer[count] == 0x08 && count > 0) {
+				count -= 1;
+				tft.print((char)0x08);
+				continue;
+			}
+			else if (buffer[count] != 0x08) {
+				tft.print(buffer[count]);
+				count += 1;
+			}
+		}
+	}
+	buffer[count] = '\0';
+	if (Serial.available() > 0) Serial.read();
+	else if (Serial1.available() > 0) Serial1.read();
+	tft.println();
+}
+int fscan(char* buffer, const char* format, ...) {
+	scan(buffer);
+	va_list args = 0;
+	va_start(args, format);
+	int result = vsscanf(buffer, format, args);
+	va_end(args);
+	return result;
+}
+#endif
